4fed579a1e25415dfd94754ca105b220d8845d36
sslash <michaelgunnulfsen@gmail.com>
2016-05-27 09:44:15 +0200

    minor tweaks to startup script

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 'use strict';
+var pjson = require('./package.json');
 
 let server = require('./config/server');
+const logger = require('./lib/helpers/logger').createLogger(__filename);
 
 server.start((err) => {
     if (err) {
+        logger.warn('Failed to run server ', err);
         throw err;
     }
-    console.log('Server running at ' + server.info.uri);
+    logger.info({what: 'Server running', uri: server.info.uri, version: pjson.version});
 });
