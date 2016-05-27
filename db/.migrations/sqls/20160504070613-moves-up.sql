3563a481d456fd7eea511bb607e000d0742f9afe
sslash <michaelgunnulfsen@gmail.com>
2016-05-27 09:11:38 +0200

    ready for deploy

--- db/.migrations/sqls/20160504070613-moves-up.sql
+++ db/.migrations/sqls/20160504070613-moves-up.sql
@@ -1,6 +1,7 @@
 create table IF NOT EXISTS moves (
     id serial primary key,
     title varchar(100),
+    gif varchar(250),
     description text,
     created timestamptz not null default now(),
     updated timestamptz not null default now()
